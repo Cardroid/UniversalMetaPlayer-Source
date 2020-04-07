8e5fa6209b08be15aef54f4edacef1539f700f77
CarbonSIX <number1dh@gmail.com>
2020-04-08 02:31:56 +0900

    오타 수정 Typo correction

--- CustomMediaPlayer/Option/OptionPage/BasicPage.xaml.cs
+++ CustomMediaPlayer/Option/OptionPage/BasicPage.xaml.cs
@@ -15,7 +15,7 @@ namespace CustomMediaPlayer.Option.OptionPage
 
             #region 옵션 내용 및 툴팁 초기화
             MediaOpeningPlayCheckbox.Content = "미디어 자동 재생";
-            MediaOpeningPlayCheckbox.ToolTip = "미디어 파일이 열리면,\n자동으로 재생합니다.";
+            MediaOpeningPlayCheckbox.ToolTip = "미디어 파일이 열리면, 자동으로 재생합니다.";
 
             LastSongSaveCheckbox.Content = "마지막 미디어 이어서 재생";
             LastSongSaveCheckbox.ToolTip = "프로그램이 종료될 때 로드되어 있는 미디어의 위치를 저장하고,\n" +
--- CustomMediaPlayer/Option/OptionPage/KeyOptionPage.xaml.cs
+++ CustomMediaPlayer/Option/OptionPage/KeyOptionPage.xaml.cs
@@ -26,8 +26,8 @@ namespace CustomMediaPlayer.Option.OptionPage
 
             // 옵션 내용 설정
             KeyHookOptionCheckbox.Content = "원격 키보드 단축키 설정";
-            KeyHookOptionCheckbox.ToolTip = "창이 활성화 되어있지 않아도, 미디어 버튼 컨트롤 버튼을 통해 컨트롤 할 수 있습니다." + "\n\n" +
-                                            "관리자권한이 없으면 일부 윈도우에서 키 입력을 감지하지 못할 수 있습니다.";
+            KeyHookOptionCheckbox.ToolTip = "창이 활성화 되어있지 않아도, 미디어 컨트롤 버튼을 통해 컨트롤 할 수 있습니다." + "\n\n" +
+                                            "관리자 권한이 없으면 일부 윈도우에서 키 입력을 감지하지 못할 수 있습니다.";
             #endregion
 
             #region 이벤트 연결
