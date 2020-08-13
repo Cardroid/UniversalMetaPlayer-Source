78826c043e78f8afef5c88890b580c929e9be14e
CarbonSIX <number1dh@gmail.com>
2020-08-14 02:48:03 +0900

    버전 확인 시스템 작성

--- UniversalMetaPlayer_UpdateClient/Program.cs
+++ UniversalMetaPlayer_UpdateClient/Program.cs
@@ -1,6 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Timers;
 
 using Octokit;
 
@@ -8,21 +14,148 @@ namespace UMP.UpdateClient
 {
   class Program
   {
-    static void Main(string[] args)
+    static bool exitSystem = false;
+
+    #region Trap application termination
+    [DllImport("Kernel32")]
+    private static extern bool SetConsoleCtrlHandler(EventHandler handler, bool add);
+
+    private delegate bool EventHandler(CtrlType sig);
+    static EventHandler _handler;
+
+    enum CtrlType
     {
-      GetVersion();
-      Console.ReadLine();
+      CTRL_C_EVENT = 0,
+      CTRL_BREAK_EVENT = 1,
+      CTRL_CLOSE_EVENT = 2,
+      CTRL_LOGOFF_EVENT = 5,
+      CTRL_SHUTDOWN_EVENT = 6
     }
 
-    public static async void GetVersion()
+    private static bool Handler(CtrlType sig)
     {
+      Console.WriteLine("[업데이트 취소 요청 입력됨]");
+      Thread.Sleep(1000);
+
+      //allow main to run off
+      exitSystem = true;
+
+      //shutdown right away so there are no lingering threads
+      Environment.Exit(-1);
+
+      return true;
+    }
+    #endregion
+
+    static void Main()
+    {
+      Console.Write("초기화 중...");
+      Console.Title = "UniversalMetaPlayer Auto Update Client";
+
+      // Some biolerplate to react to close window event, CTRL-C, kill, etc
+      _handler += new EventHandler(Handler);
+      SetConsoleCtrlHandler(_handler, true);
+
+      //start your multi threaded program here
+      Program p = new Program();
+      Console.WriteLine("완료");
+      p.Start();
+
+      //hold the console so it doesn’t run off the end
+      while (!exitSystem)
+      {
+        Thread.Sleep(500);
+      }
+
+      Console.WriteLine("프로그램 종료 중...");
+#if DEBUG
+      Console.WriteLine();
+      Thread.Sleep(1000);
+      Console.Write("5...");
+      Thread.Sleep(1000);
+      Console.Write("4...");
+      Thread.Sleep(1000);
+      Console.Write("3...");
+      Thread.Sleep(1000);
+      Console.Write("2...");
+      Thread.Sleep(1000);
+      Console.Write("1...");
+      Thread.Sleep(1000);
+#endif
+      Console.Write("End");
+    }
+
+    public async void Start()
+    {
+      Console.WriteLine("업데이트를 시작합니다");
+
+      if (Utility.CheckForInternetConnection())
+      {
+        Console.WriteLine("현재 버전 정보 가져오는 중...");
+        GetCurrentVersion();
+        Console.WriteLine("완료");
+        Console.WriteLine("새 버전 정보 가져오는 중...");
+        await GetNewVersion();
+        Console.WriteLine("완료");
+        if(TargetVerstion != null && TargetVerstion.CompareTo(ProgramVerstion) > 0)
+        {
+          Console.WriteLine("업데이트가 필요합니다");
+          // TODO 업데이트 메소드 작성
+          Console.WriteLine("업데이트 시작");
+        }
+        else
+        {
+          Console.WriteLine("업데이트가 필요하지 않습니다");
+        }
+      }
+      else
+      {
+        Console.WriteLine("네트워크를 사용할 수 없습니다");
+        Thread.Sleep(3000);
+      }
+      exitSystem = true;
+    }
+
+    private Dictionary<string, string> Assets { get; set; }
+    private string OS_Bit => Environment.Is64BitOperatingSystem ? "x64" : "x86";
+    private Version TargetVerstion { get; set; } = null;
+    private Version ProgramVerstion { get; set; } = null;
+
+    private bool GetCurrentVersion()
+    {
+      string file = string.Empty;
+      string[] files;
+
+      try { files = Directory.GetFiles(Environment.CurrentDirectory, "*.exe"); }
+      catch { return false; }
+
+      if (files == null || files.Length <= 0)
+        return false;
+
+      for (int i = 0; i < files.Length; i++)
+      {
+        if (FileVersionInfo.GetVersionInfo(files[i]).ProductName == "Universal Meta Player")
+          file = files[i];
+      }
+
+      if (string.IsNullOrWhiteSpace(file))
+        return false;
+
+      ProgramVerstion = new Version(FileVersionInfo.GetVersionInfo(file).FileVersion);
+
+      if (ProgramVerstion != null)
+        Console.WriteLine($"Current Version : [{ProgramVerstion}]");
+      else
+        Console.WriteLine("Current Version : [정보 없음]");
+
+      return true;
+    }
+
+    private async Task<bool> GetNewVersion()
+    {
+      Assets = new Dictionary<string, string>();
       var client = new GitHubClient(new ProductHeaderValue("UniversalMetaPlayer"));
 
-      string ver;
-      Dictionary<string, string> assets = new Dictionary<string, string>();
-      string latestver = string.Empty;
-
-      Exception apierror = null;
       try
       {
         var releases = await client.Repository.Release.GetLatest("Cardroid", "UniversalMetaPlayer");
@@ -30,29 +163,25 @@ namespace UMP.UpdateClient
         if (releases == null)
           throw new ArgumentNullException();
 
-        latestver = releases.TagName;
+        TargetVerstion = new Version(releases.TagName);
 
         for (int i = 0; i < releases.Assets.Count; i++)
-          assets.Add(releases.Assets[i].Name, releases.Assets[i].BrowserDownloadUrl);
+          Assets.Add(releases.Assets[i].Name, releases.Assets[i].BrowserDownloadUrl);
       }
-      catch (Exception e)
+      catch
       {
-        apierror = e;
+        return false;
       }
 
-      if (apierror != null)
-      {
-        Console.WriteLine("Error");
-        Console.WriteLine(apierror);
-        return;
-      }
-
-      Console.WriteLine(latestver);
-      foreach (var item in assets.Keys)
+      Console.WriteLine($"Target Version : [{TargetVerstion}]");
+#if DEBUG
+      foreach (var item in Assets.Keys)
       {
         Console.WriteLine(item);
-        Console.WriteLine(assets.TryGetValue(item, out string value) ? value : "Null");
+        Console.WriteLine(Assets.TryGetValue(item, out string value) ? value : "Null");
       }
+#endif
+      return true;
     }
   }
 }
---
+++ UniversalMetaPlayer_UpdateClient/Utility.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UMP.UpdateClient
+{
+  public static class Utility
+  {
+    /// <summary>
+    /// ZIP 파일 추출하기
+    /// </summary>
+    /// <param name="zipFilePath">ZIP 파일 경로</param>
+    /// <param name="targetFilePath">백업 폴더</param>
+    public static async Task<bool> ExtractZIPFileAsync(string zipFilePath, string targetFilePath)
+    {
+      bool result = false;
+      await Task.Run(() =>
+      {
+        if (File.Exists(zipFilePath))
+        {
+          try
+          {
+            using (ZipArchive zipArchive = ZipFile.OpenRead(zipFilePath))
+            {
+              // 압축 풀 폴더에 이미 존재하면 삭제
+              if (Directory.Exists(Path.Combine(targetFilePath, zipArchive.Entries[0].FullName)))
+                Directory.Delete(Path.Combine(targetFilePath, zipArchive.Entries[0].FullName), true);
+
+              foreach (ZipArchiveEntry zipArchiveEntry in zipArchive.Entries)
+              {
+                string folderPath = Path.GetDirectoryName(Path.Combine(targetFilePath, zipArchiveEntry.FullName));
+
+                if (!Directory.Exists(folderPath))
+                  Directory.CreateDirectory(folderPath);
+
+                // 경로명은 제외
+                if (!string.IsNullOrWhiteSpace(zipArchiveEntry.FullName) && !zipArchiveEntry.FullName.EndsWith("\\") && !zipArchiveEntry.FullName.EndsWith("/"))
+                  zipArchiveEntry.ExtractToFile(Path.Combine(targetFilePath, zipArchiveEntry.FullName));
+              }
+            }
+            result = true;
+          }
+          catch
+          {
+            result = false;
+          }
+        }
+        else
+          result = false;
+      });
+      return result;
+    }
+
+    /// <summary>
+    /// 인터넷에 연결되었는지 체크합니다
+    /// </summary>
+    /// <returns>true = 인터넷 사용가능, false = 인터넷 사용불가능</returns>
+    public static bool CheckForInternetConnection()
+    {
+      try
+      {
+        using (var client = new WebClient())
+        using (client.OpenRead("http://clients3.google.com/generate_204"))
+        { return true; }
+      }
+      catch { return false; }
+    }
+  }
+}
